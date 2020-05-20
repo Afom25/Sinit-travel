651d2d9c9fe5df1804bdb8cec6e8e139bf41675b
hidri <agthidri24@yahoo.com>
2020-05-20 12:49:43 +0300

    thsi commit will add the components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>SINIT TRAVEL </h1>
+    </div>
+  );
+}
+
+export default App;
